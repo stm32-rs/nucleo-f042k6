9004407751f6fa85a77e0a0547b69f634ff42d87
Daniel Egger <daniel@eggers-club.de>
2019-01-05 22:44:32 +0100

    Added an i2c_scanner example
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

---
+++ examples/i2c_scanner.rs
@@ -0,0 +1,125 @@
+#![no_main]
+#![no_std]
+
+use panic_halt;
+
+use stm32f0xx_hal as hal;
+
+use cortex_m_rt::entry;
+
+use crate::hal::gpio::{gpiof::PF0, gpiof::PF1, Alternate, AF1};
+use crate::hal::i2c::*;
+use crate::hal::prelude::*;
+use crate::hal::serial::Serial;
+use crate::hal::stm32::{self, interrupt, Interrupt::USART2};
+
+use core::cell::RefCell;
+use core::fmt::Write;
+use core::ops::DerefMut;
+use cortex_m::interrupt::Mutex;
+
+// Make some peripherals globally available
+struct Shared {
+    i2c: hal::i2c::I2c<stm32::I2C1, PF1<Alternate<AF1>>, PF0<Alternate<AF1>>>,
+    rx: hal::serial::Rx<stm32::USART2>,
+    tx: hal::serial::Tx<stm32::USART2>,
+}
+
+static SHARED: Mutex<RefCell<Option<Shared>>> = Mutex::new(RefCell::new(None));
+
+#[entry]
+fn main() -> ! {
+    if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), cortex_m::Peripherals::take()) {
+        let gpioa = p.GPIOA.split();
+        let gpiof = p.GPIOF.split();
+        let rcc = p.RCC.constrain();
+        let clocks = rcc.cfgr.freeze();
+        let mut nvic = cp.NVIC;
+
+        let scl = gpiof
+            .pf1
+            .into_alternate_af1()
+            .internal_pull_up(true)
+            .set_open_drain();
+        let sda = gpiof
+            .pf0
+            .into_alternate_af1()
+            .internal_pull_up(true)
+            .set_open_drain();
+
+        // Setup I2C1
+        let i2c = I2c::i2c1(p.I2C1, (scl, sda), 100.khz());
+
+        // USART2 at PA2 (TX) and PA15(RX) is connectet to ST-Link
+        let tx = gpioa.pa2.into_alternate_af1();
+        let rx = gpioa.pa15.into_alternate_af1();
+
+        // Set up our serial port for output
+        let mut serial = Serial::usart2(p.USART2, (tx, rx), 115_200.bps(), clocks);
+
+        // Enable USART2 interrupt on received input
+        serial.listen(hal::serial::Event::Rxne);
+        let (mut tx, rx) = serial.split();
+
+        // Enable USART2 interrupt and clear any pending interrupts
+        nvic.enable(USART2);
+        cortex_m::peripheral::NVIC::unpend(USART2);
+
+        // Print a welcome message
+        tx.write_str("\r\nWelcome to the I2C scanner. Enter any character to start scan.\r\n")
+            .ok();
+
+        // Move all components under Mutex supervision
+        cortex_m::interrupt::free(move |cs| {
+            *SHARED.borrow(cs).borrow_mut() = Some(Shared { i2c, rx, tx });
+        });
+    }
+
+    loop {
+        continue;
+    }
+}
+
+// The IRQ handler triggered by a received character in USART buffer, this will conduct our I2C
+// scan when we receive anything
+#[interrupt]
+fn USART2() {
+    cortex_m::interrupt::free(|cs| {
+        // Obtain all Mutex protected resources
+        if let Some(ref mut shared) = SHARED.borrow(cs).borrow_mut().deref_mut() {
+            let tx = &mut shared.tx;
+            let rx = &mut shared.rx;
+            let i2c = &mut shared.i2c;
+
+            /* Read the character that triggered the interrupt from the USART */
+            while rx.read().is_ok() {}
+
+            /* Output address schema for tried addresses */
+            let _ = tx.write_str("\r\n");
+            let _ = tx.write_str(
+                "0               1               2               3               4               5               6               7\r\n",
+                );
+            let _ = tx.write_str(
+                "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF\r\n",
+                );
+
+            // Execute scanning once for each valid I2C address
+            for addr in 0..=0x7f {
+                let res = i2c.write(addr, &[0]);
+
+                // If we received a NACK there's no device on the attempted address
+                let _ = tx.write_str(match res {
+                    Err(Error::NACK) => ".",
+                    _ => "Y",
+                });
+            }
+
+            let _ = tx.write_str(
+                "\r\n\r\nScan done.\r\n'Y' means a device was found on the I2C address above.\r\n'.' means no device found on that address.\r\nPlease enter any character to start a new scan.\r\n",
+                );
+        }
+
+        // Clear interrupt flag
+        cortex_m::peripheral::NVIC::unpend(USART2);
+    });
+}
